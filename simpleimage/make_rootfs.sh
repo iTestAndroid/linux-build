1d255fbe94381f59f8434cf090f5da6776f29aa3
iTestAndroid <itestandroid2020@gmail.com>
2019-03-16 21:13:56 -0400

    Update make_rootfs.sh

--- simpleimage/make_rootfs.sh
+++ simpleimage/make_rootfs.sh
@@ -125,6 +125,7 @@ echo "OK"
 # Add qemu emulation.
 cp /usr/bin/qemu-aarch64-static "$DEST/usr/bin"
 cp /usr/bin/qemu-arm-static "$DEST/usr/bin"
+echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > "$DEST/usr/lib/binfmt.d/qemu-aarch64-static.conf"
 
 # Prevent services from starting
 cat > "$DEST/usr/sbin/policy-rc.d" <<EOF
